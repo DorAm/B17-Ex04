e6fdb2ad5f621e9e52340268f2b6a8f7a5c2c58e
DorAmar <bndor.am@gmail.com>
2017-06-12 20:09:27 +0300

    copied previous files

---
+++ B17 Ex04 Dor Amar 301113403 Ori Reshef 203319900/Ex04.Menus.Delegates/MenuItem.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex04.Menus.Interfaces
+{
+    class MenuItem
+    {
+        private MenuItem m_ParentItem;
+        private List<MenuItem> m_ChildItems;
+        private string m_Option;
+        private bool m_IsActionItem = false;
+        private IPickObserver m_PickObserver;
+
+        // Getters 
+        public string Option { get => m_Option; }
+        public List<MenuItem> ChildItems { get => m_ChildItems; }
+        public bool IsActionItem { get => m_IsActionItem; }
+        public IPickObserver PickObserver { get => m_PickObserver; }
+
+        // Methods
+        public MenuItem(string i_Option, MenuItem i_ParentItem)
+        {
+            m_Option = i_Option;
+            m_ParentItem = i_ParentItem;
+            m_ChildItems = new List<MenuItem>();
+
+            //if (m_ParentItem != null)
+            //{
+            //    m_ParentItem.AddChild(this);
+            //}
+        }
+
+        public MenuItem(string i_Option, MenuItem i_ParentItem, IPickObserver i_PickObserver)
+        : this(i_Option, i_ParentItem)
+        {
+            m_PickObserver = i_PickObserver;
+            m_IsActionItem = true;
+        }
+
+        public void show()
+        {
+            Console.Clear();
+            Console.WriteLine("========== {0} =========={1}", m_Option);
+            displayOptions();
+            ushort choice = getUsersChoice();
+            if (choice == 0)
+            {
+                //if Exit was chosen return to main and exit
+                if (m_ParentItem == null)
+                {
+                    return;
+                }
+                else
+                {
+                    m_ParentItem.show();
+                }
+            }
+            else
+            {
+                MenuItem chosenMenuItem = m_ChildItems[choice - 1];
+                if (chosenMenuItem.IsActionItem)
+                {
+                    chosenMenuItem.PickObserver.ReportPicked(chosenMenuItem.Option);                        
+                }
+                else
+                {
+                    chosenMenuItem.show();
+                }
+            }
+        }
+
+        public void displayOptions()
+        {
+            if (IsActionItem == false)
+            {
+                Console.WriteLine("0. {0}", m_ParentItem == null ? "Exit" : "Back");
+                ushort index = 1;
+                foreach (MenuItem item in m_ChildItems)
+                {
+                    Console.WriteLine("{0}. {1}", index, item.Option);
+                    index += 1;
+                }
+                Console.WriteLine("Please choose one of the above options");
+                Console.WriteLine("======================================");
+
+            }
+        }
+
+        private ushort getUsersChoice()
+        {
+            string input = Console.ReadLine();
+            ushort usersChoice = ushort.Parse(input);
+            while (isValidInput(usersChoice) == false)
+            {
+                Console.WriteLine("Invalid Input, Please re-enter your choice:");
+                usersChoice = ushort.Parse(input);
+            }
+            return usersChoice;
+        }
+
+        private bool isValidInput(ushort i_UsersChoice)
+        {
+            return 0 <= i_UsersChoice && i_UsersChoice <= m_ChildItems.Count;
+        }
+    }
+}
